a6857936cbd1803ec9f577c9c8c169a90b7f26a2
Casey <casey.aristide@gmail.com>
2022-11-26 23:54:53 -0500

    created a varible with properties that specify what the user can choose from

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Assignment Code
+//Assignment code
 var generateBtn = document.querySelector("#generate");
+var passwordCriteria = {
+  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  numeric: [0,1,2,3,4,5,6,7,8,9],
+  specialChar: [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\"","]","^","_","`","{","|","}","~","\\"]
+} 
 
 // Write password to the #password input
 function writePassword() {
