6512d6d1701d9496e31ac61a37f659c5b8cb0acb
Casey <casey.aristide@gmail.com>
2022-11-27 20:10:01 -0500

    Created variables with empty arrays and use .concat to conjoin users preferred characters(the arrays) athe empty array. I then created a for loop to randomly select users preferred amount of characters from that new array.

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var lowercase= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 var uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric= [0,1,2,3,4,5,6,7,8,9];
 var specialChar= [" ","!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\"","]","^","_","`","{","|","}","~","\\"];
-var userPref = []
+var userPref = [];
+var password = [];
 // Write password to the #password input
 function writePassword() {
-  function generatePassword() {
     var passLength = window.prompt("How many characters would you like? Must be at least 8 and cannot exceed 128");
     if (passLength<8 || passLength>128) {
       window.alert("Password must be at least 8 characters and must not exceed 128 characters.")
@@ -15,26 +15,30 @@ function writePassword() {
     };
     var userChoiceOne = window.prompt("Would you like to include lowercase characters? Please say YES or NO.");
     if (userChoiceOne === "YES") {
-      userPref.concat(lowercase);
+      userPref= userPref.concat(lowercase);
     };
     var userChoiceTwo = window.prompt("Would you like to include uppercase characters? Please say YES or NO");
     if (userChoiceTwo === "YES") {
-      userPref.concat(uppercase);
+      userPref= userPref.concat(uppercase);
     };
     var userChoiceThree = window.prompt("Would you like to include numbers? Please say YES or NO");
     if (userChoiceThree === "YES") {
-      userPref.concat(numeric);
+      userPref= userPref.concat(numeric);
     };
     var userChoiceFour = window.prompt("Would you like to include special characters? Please say YES or NO");
     if (userChoiceFour === "YES") {
-      userPref.concat(specialChar);
+      userPref= userPref.concat(specialChar);
     };
     if (userPref.length === 0) {
-      window.concat("Password criteria must contain at least one choice. Please type 'Yes' for at least one choice.")
+      window.alert("Password criteria must contain at least one choice. Please select at least one choice.")
       return;
     }
-  }
-  var password = generatePassword();
+    for (var i= 0; i<passLength; i++) {
+      var randomIndex= Math.floor(Math.random() * userPref.length);
+      var random = userPref[randomIndex];
+      password = password.concat(random)
+    }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -42,6 +46,7 @@ function writePassword() {
 
   console.log(password);
   console.log(userPref); 
+  console.log(writePassword);
 }
 
 // Add event listener to generate button
